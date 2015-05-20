5384deb29a21d530578b471d3d73554ee8eadaf2
Roman Senn <roman.l.senn@gmail.com>
2015-05-20 18:39:34 +0200

    LC meter pic16f876A (diagram and source code)

---
+++ main.h
@@ -0,0 +1,27 @@
+#include <htc.h>	// Required to interface with delay routines
+#ifndef _XTAL_FREQ
+	#define _XTAL_FREQ 20000000
+#endif
+__CONFIG(FOSC_HS & WDTE_OFF & PWRTE_ON & BOREN_ON);
+
+#include "lcd3310.h"
+
+#define Ccal			1000		//pF
+#define gate_period		100			//ms
+#define pi				3.14159
+#define LC_select		RB0			//L or C select (from DPDT switch)
+#define lc_tris()		TRISB0 = 1	//as input
+#define add_ccal()		RC0 = 1		//relay on
+#define remove_ccal()	RC0 = 0		//relay off
+#define relay_tris()	TRISC0 = 0	//as output to drive the relay coil
+
+double F1, F2, F3;
+
+void initialize(void);
+unsigned int measure_freq(void);
+void calibrate(void);
+void measure_capacitance(void);
+void measure_inductance(void);
+void delay10ms(unsigned int period_10ms);
+
+
