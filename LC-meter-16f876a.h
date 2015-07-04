905cfa0f6dd96e53fb7842866674c781fecee92b
Roman Senn <roman.l.senn@gmail.com>
2015-07-04 23:00:56 +0200

    ...

---
+++ LC-meter-16f876a.h
@@ -0,0 +1,14 @@
+#include <16F876A.h>
+#device adc=16
+
+#FUSES NOWDT                 	//No Watch Dog Timer
+#FUSES HS                    	//High speed Osc (> 4mhz for PCM/PCH) (>10mhz for PCD)
+#FUSES PUT                   	//Power Up Timer
+#FUSES NOLVP                 	//No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
+
+#use delay(clock=184320000)
+
+#define LED PIN_None
+#define DELAY 1000
+
+
