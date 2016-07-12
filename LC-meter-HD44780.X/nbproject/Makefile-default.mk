131e3bb7a93135588c47de78c2bb1d746760479d
Roman Senn <roman.l.senn@gmail.com>
2016-07-12 09:18:03 +0200

    ...

--- LC-meter-HD44780.X/nbproject/Makefile-default.mk
+++ LC-meter-HD44780.X/nbproject/Makefile-default.mk
@@ -45,17 +45,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=../delay.c ../display.c ../LC_meter.c ../lcd3310.c ../lcd44780.c ../LC-meter-HD44780.c
+SOURCEFILES_QUOTED_IF_SPACED=../delay.c ../lcd44780.c ../LC-meter-HD44780.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1472/delay.p1 ${OBJECTDIR}/_ext/1472/display.p1 ${OBJECTDIR}/_ext/1472/LC_meter.p1 ${OBJECTDIR}/_ext/1472/lcd3310.p1 ${OBJECTDIR}/_ext/1472/lcd44780.p1 ${OBJECTDIR}/_ext/1472/LC-meter-HD44780.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1472/delay.p1.d ${OBJECTDIR}/_ext/1472/display.p1.d ${OBJECTDIR}/_ext/1472/LC_meter.p1.d ${OBJECTDIR}/_ext/1472/lcd3310.p1.d ${OBJECTDIR}/_ext/1472/lcd44780.p1.d ${OBJECTDIR}/_ext/1472/LC-meter-HD44780.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1472/delay.p1 ${OBJECTDIR}/_ext/1472/lcd44780.p1 ${OBJECTDIR}/_ext/1472/LC-meter-HD44780.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1472/delay.p1.d ${OBJECTDIR}/_ext/1472/lcd44780.p1.d ${OBJECTDIR}/_ext/1472/LC-meter-HD44780.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/_ext/1472/delay.p1 ${OBJECTDIR}/_ext/1472/display.p1 ${OBJECTDIR}/_ext/1472/LC_meter.p1 ${OBJECTDIR}/_ext/1472/lcd3310.p1 ${OBJECTDIR}/_ext/1472/lcd44780.p1 ${OBJECTDIR}/_ext/1472/LC-meter-HD44780.p1
+OBJECTFILES=${OBJECTDIR}/_ext/1472/delay.p1 ${OBJECTDIR}/_ext/1472/lcd44780.p1 ${OBJECTDIR}/_ext/1472/LC-meter-HD44780.p1
 
 # Source Files
-SOURCEFILES=../delay.c ../display.c ../LC_meter.c ../lcd3310.c ../lcd44780.c ../LC-meter-HD44780.c
+SOURCEFILES=../delay.c ../lcd44780.c ../LC-meter-HD44780.c
 
 
 CFLAGS=
@@ -89,30 +89,6 @@ ${OBJECTDIR}/_ext/1472/delay.p1: ../delay.c  nbproject/Makefile-${CND_CONF}.mk
 	@-${MV} ${OBJECTDIR}/_ext/1472/delay.d ${OBJECTDIR}/_ext/1472/delay.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/_ext/1472/delay.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
-${OBJECTDIR}/_ext/1472/display.p1: ../display.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} "${OBJECTDIR}/_ext/1472" 
-	@${RM} ${OBJECTDIR}/_ext/1472/display.p1.d 
-	@${RM} ${OBJECTDIR}/_ext/1472/display.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=icd3  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 -I".." -I"." --warn=-3 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf --stack=compiled "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/1472/display.p1  ../display.c 
-	@-${MV} ${OBJECTDIR}/_ext/1472/display.d ${OBJECTDIR}/_ext/1472/display.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/_ext/1472/display.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
-	
-${OBJECTDIR}/_ext/1472/LC_meter.p1: ../LC_meter.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} "${OBJECTDIR}/_ext/1472" 
-	@${RM} ${OBJECTDIR}/_ext/1472/LC_meter.p1.d 
-	@${RM} ${OBJECTDIR}/_ext/1472/LC_meter.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=icd3  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 -I".." -I"." --warn=-3 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf --stack=compiled "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/1472/LC_meter.p1  ../LC_meter.c 
-	@-${MV} ${OBJECTDIR}/_ext/1472/LC_meter.d ${OBJECTDIR}/_ext/1472/LC_meter.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/_ext/1472/LC_meter.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
-	
-${OBJECTDIR}/_ext/1472/lcd3310.p1: ../lcd3310.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} "${OBJECTDIR}/_ext/1472" 
-	@${RM} ${OBJECTDIR}/_ext/1472/lcd3310.p1.d 
-	@${RM} ${OBJECTDIR}/_ext/1472/lcd3310.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=icd3  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 -I".." -I"." --warn=-3 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf --stack=compiled "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/1472/lcd3310.p1  ../lcd3310.c 
-	@-${MV} ${OBJECTDIR}/_ext/1472/lcd3310.d ${OBJECTDIR}/_ext/1472/lcd3310.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/_ext/1472/lcd3310.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
-	
 ${OBJECTDIR}/_ext/1472/lcd44780.p1: ../lcd44780.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/_ext/1472" 
 	@${RM} ${OBJECTDIR}/_ext/1472/lcd44780.p1.d 
@@ -137,30 +113,6 @@ ${OBJECTDIR}/_ext/1472/delay.p1: ../delay.c  nbproject/Makefile-${CND_CONF}.mk
 	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 -I".." -I"." --warn=-3 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf --stack=compiled "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/1472/delay.p1  ../delay.c 
 	@-${MV} ${OBJECTDIR}/_ext/1472/delay.d ${OBJECTDIR}/_ext/1472/delay.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/_ext/1472/delay.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
-	
-${OBJECTDIR}/_ext/1472/display.p1: ../display.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} "${OBJECTDIR}/_ext/1472" 
-	@${RM} ${OBJECTDIR}/_ext/1472/display.p1.d 
-	@${RM} ${OBJECTDIR}/_ext/1472/display.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 -I".." -I"." --warn=-3 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf --stack=compiled "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/1472/display.p1  ../display.c 
-	@-${MV} ${OBJECTDIR}/_ext/1472/display.d ${OBJECTDIR}/_ext/1472/display.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/_ext/1472/display.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
-	
-${OBJECTDIR}/_ext/1472/LC_meter.p1: ../LC_meter.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} "${OBJECTDIR}/_ext/1472" 
-	@${RM} ${OBJECTDIR}/_ext/1472/LC_meter.p1.d 
-	@${RM} ${OBJECTDIR}/_ext/1472/LC_meter.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 -I".." -I"." --warn=-3 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf --stack=compiled "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/1472/LC_meter.p1  ../LC_meter.c 
-	@-${MV} ${OBJECTDIR}/_ext/1472/LC_meter.d ${OBJECTDIR}/_ext/1472/LC_meter.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/_ext/1472/LC_meter.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
-	
-${OBJECTDIR}/_ext/1472/lcd3310.p1: ../lcd3310.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} "${OBJECTDIR}/_ext/1472" 
-	@${RM} ${OBJECTDIR}/_ext/1472/lcd3310.p1.d 
-	@${RM} ${OBJECTDIR}/_ext/1472/lcd3310.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 -I".." -I"." --warn=-3 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf --stack=compiled "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/1472/lcd3310.p1  ../lcd3310.c 
-	@-${MV} ${OBJECTDIR}/_ext/1472/lcd3310.d ${OBJECTDIR}/_ext/1472/lcd3310.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/_ext/1472/lcd3310.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 ${OBJECTDIR}/_ext/1472/lcd44780.p1: ../lcd44780.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/_ext/1472" 
