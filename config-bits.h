69a323e2c0491b6f973871d3c93d9c3cc9660285
Roman Senn <roman.l.senn@gmail.com>
2016-10-12 15:20:53 +0200

    ...

---
+++ config-bits.h
@@ -0,0 +1,33 @@
+#ifndef CONFIG_BITS_H
+#define CONFIG_BITS_H 1
+
+#ifdef __10f206
+# include "config-10f206.h"
+#endif // defined __10f206
+
+#ifdef __12f1840
+# include "config-12f1840.h"
+#endif // defined __12f1840
+
+#ifdef __16f876a
+# include "config-16f876a.h"
+#endif // defined __16f876a
+
+#ifdef __18f252
+# include "config-18f252.h"
+#endif // defined __18f252
+
+#ifdef __18f2550
+# include "config-18f2550.h"
+#endif // defined __18f2550
+
+#ifdef __18f25k22
+# include "config-18f25k22.h"
+#endif // defined __18f25k22
+
+#ifdef __18f25k50
+# include "config-18f25k50.h"
+#endif // defined __18f25k50
+
+#endif // defined CONFIG_BITS_H
+ 
