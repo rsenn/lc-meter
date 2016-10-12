317323bbc3c8f84c97e2ef359577055d54ef33e8
Roman Senn <roman.l.senn@gmail.com>
2016-10-12 15:31:25 +0200

    ...

--- vars.sh
+++
@@ -1,4 +0,0 @@
-hdr='c:/Program Files (x86)/HI-TECH Software/PICC/9.83/include/pic16f876a.h'
-hdr_xc8='c:/Program Files (x86)/Microchip/xc8/v1.34/include/pic16f876a.h'
-cc='c:/Program Files (x86)/HI-TECH Software/PICC/9.83/bin/picc.exe'
-cc_xc8='c:/Program Files (x86)/Microchip/xc8/v1.34/bin/picc.exe'
