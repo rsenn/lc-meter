bd2a5265ff9ba2e1558f1ab96fea0fc574a23ca9
Roman Senn <roman.l.senn@gmail.com>
2016-10-19 08:50:45 +0200

    ...

--- build/mplabx/LC-meter-HD44780.X/build/default/production/_ext/1386521430/delay.p1.d
+++
@@ -1,8 +0,0 @@
- build/default/production/_ext/1386521430/delay.d  \
- build/default/production/_ext/1386521430/delay.p1:  \
- ../../../lib/delay.c  \
-../../../lib/tsmdelay.h  \
-../../../lib/delay.h  \
-../../../lib/oscillator.h  \
-../../../lib/typedef.h  \
-../../../src/device.h 
--- build/mplabx/LC-meter-HD44780.X/build/default/production/_ext/1386521430/ser.p1.d
+++
@@ -1,6 +0,0 @@
- build/default/production/_ext/1386521430/ser.d  \
- build/default/production/_ext/1386521430/ser.p1:  \
- ../../../lib/ser.c  \
-../../../lib/ser.h  \
-../../../lib/typedef.h  \
-../../../src/device.h 
