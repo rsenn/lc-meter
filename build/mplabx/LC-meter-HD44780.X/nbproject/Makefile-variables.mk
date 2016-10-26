2fd1386b989b40b426eca8f2a0ae5315cdfd2c5f
Roman Senn <roman.l.senn@gmail.com>
2016-10-26 08:52:14 +0200

    ...

--- build/mplabx/LC-meter-HD44780.X/nbproject/Makefile-variables.mk
+++ build/mplabx/LC-meter-HD44780.X/nbproject/Makefile-variables.mk
@@ -4,10 +4,17 @@
 # NOCDDL
 #
 CND_BASEDIR=`pwd`
-# default configuration
-CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=LC-meter-HD44780.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/LC-meter-HD44780.X.production.hex
-CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=lc-meter-hd44780.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/lc-meter-hd44780.x.tar
+# release configuration
+CND_ARTIFACT_DIR_release=dist/release/production
+CND_ARTIFACT_NAME_release=LC-meter-HD44780.X.production.hex
+CND_ARTIFACT_PATH_release=dist/release/production/LC-meter-HD44780.X.production.hex
+CND_PACKAGE_DIR_release=${CND_DISTDIR}/release/package
+CND_PACKAGE_NAME_release=lc-meter-hd44780.x.tar
+CND_PACKAGE_PATH_release=${CND_DISTDIR}/release/package/lc-meter-hd44780.x.tar
+# debug configuration
+CND_ARTIFACT_DIR_debug=dist/debug/production
+CND_ARTIFACT_NAME_debug=LC-meter-HD44780.X.production.hex
+CND_ARTIFACT_PATH_debug=dist/debug/production/LC-meter-HD44780.X.production.hex
+CND_PACKAGE_DIR_debug=${CND_DISTDIR}/debug/package
+CND_PACKAGE_NAME_debug=lc-meter-hd44780.x.tar
+CND_PACKAGE_PATH_debug=${CND_DISTDIR}/debug/package/lc-meter-hd44780.x.tar
--- build/mplabx/LC-meter-Nokia3310.X/nbproject/Makefile-variables.mk
+++ build/mplabx/LC-meter-Nokia3310.X/nbproject/Makefile-variables.mk
@@ -4,10 +4,17 @@
 # NOCDDL
 #
 CND_BASEDIR=`pwd`
-# default configuration
-CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=LC-meter-Nokia3310.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/LC-meter-Nokia3310.X.production.hex
-CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=lc-meter-nokia3310.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/lc-meter-nokia3310.x.tar
+# release configuration
+CND_ARTIFACT_DIR_release=dist/release/production
+CND_ARTIFACT_NAME_release=LC-meter-Nokia3310.X.production.hex
+CND_ARTIFACT_PATH_release=dist/release/production/LC-meter-Nokia3310.X.production.hex
+CND_PACKAGE_DIR_release=${CND_DISTDIR}/release/package
+CND_PACKAGE_NAME_release=lc-meter-nokia3310.x.tar
+CND_PACKAGE_PATH_release=${CND_DISTDIR}/release/package/lc-meter-nokia3310.x.tar
+# debug configuration
+CND_ARTIFACT_DIR_debug=dist/debug/production
+CND_ARTIFACT_NAME_debug=LC-meter-Nokia3310.X.production.hex
+CND_ARTIFACT_PATH_debug=dist/debug/production/LC-meter-Nokia3310.X.production.hex
+CND_PACKAGE_DIR_debug=${CND_DISTDIR}/debug/package
+CND_PACKAGE_NAME_debug=lc-meter-nokia3310.x.tar
+CND_PACKAGE_PATH_debug=${CND_DISTDIR}/debug/package/lc-meter-nokia3310.x.tar
