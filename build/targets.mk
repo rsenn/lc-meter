095f451fd7967f0301c8687ff635806f6754bba9
Roman Senn <roman.l.senn@gmail.com>
2016-11-15 20:31:09 +0100

    ...

--- build/targets.mk
+++ build/targets.mk
@@ -17,9 +17,10 @@ endif
 LC_meter_HD44780_DEFS += $(TIMER_DEFS)
 
 
-LC_meter_NOKIA3310_SOURCES = LC-meter.c lcd3310.c display.c uart.c
+LC_meter_NOKIA3310_SOURCES = LC-meter.c lcd3310.c display.c uart.c ser.c timer.c
 LC_meter_NOKIA3310_DEFS = -DUSE_NOKIA3310_LCD=1
 LC_meter_NOKIA3310_DEFS += $(TIMER_DEFS)
+LC_meter_NOKIA3310_DEFS += -DUSE_SER=1 -DUSE_TIMER_0=1
 
 
 Freq_meter_HD44780_SOURCES = Freq-meter.c lcd44780.c display.c ser.c
