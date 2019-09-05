a00a335bb066024a3305523abf7b368b4b678ead
Roman Senn <roman.l.senn@gmail.com>
2019-09-05 11:52:39 +0200

    ...

--- build/mplabx-pic16f/LC_meter_16f876a_xc8.X/build/default/production/_ext/1386521430/format.p1.d
+++
@@ -1,6 +0,0 @@
- build/default/production/_ext/1386521430/format.d  \
- build/default/production/_ext/1386521430/format.p1:  \
- ../../../lib/format.c  \
-../../../lib/../src/device.h  \
-../../../lib/format.h  \
-../../../lib/typedef.h 
--- build/mplabx-pic18f/LC_meter_18f2550_htc.X/build/default/production/_ext/1386521430/ser.p1.d
+++
@@ -1,7 +0,0 @@
-build/default/production/_ext/1386521430/ser.p1:	\
-	../../../lib/../src/device.h	\
-	../../../lib/uart.h	\
-	../../../lib/const.h	\
-	../../../lib/ser.h	\
-	../../../lib/oscillator.h	\
-	../../../lib/typedef.h	\
