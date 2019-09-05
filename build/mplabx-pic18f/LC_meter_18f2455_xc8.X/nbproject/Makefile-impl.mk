8b2f05b2b8848b273d8390ee8cb3b8ca95d16fb1
Roman Senn <roman.l.senn@gmail.com>
2019-09-05 11:44:59 +0200

    ...

--- build/mplabx-pic18f/LC_meter_18f2455_xc8.X/nbproject/Makefile-impl.mk
+++ build/mplabx-pic18f/LC_meter_18f2455_xc8.X/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=LC_meter_18f252_xc8.X
+PROJECTNAME=LC_meter_18f2455_xc8.X
 
 # Active Configuration
 DEFAULTCONF=default
