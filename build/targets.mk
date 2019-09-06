2e3207d7886c6de38bb851763b44642d38a8a2a6
Roman Senn <roman.l.senn@gmail.com>
2019-09-06 17:18:31 +0200

    ...

--- build/targets.mk
+++ build/targets.mk
@@ -2,7 +2,7 @@ COMMON_SOURCES = format.c timer.c delay.c
 
 
 #LC_meter_HD44780_SOURCES = LC-meter-HD44780.c lcd44780.c uart.c ser.c
-LC_meter_HD44780_SOURCES = LC-meter.c lcd44780.c print.c ser.c
+LC_meter_HD44780_SOURCES = LC-meter.c measure.c lcd44780.c print.c ser.c
 #$(BUILDDIR)LC_meter_HD44780_$(BUILD_ID).hex: DEFINES += USE_HD44780_LCD=1
 $(BUILDDIR)LC_meter_HD44780_$(BUILD_ID).hex: DEFINES += USE_SER=1 USE_TIMER0=1
 
---
+++ src/measure.h
@@ -0,0 +1,8 @@
+#ifndef MEASURE_H
+#define MEASURE_H
+
+unsigned short measure_freq(void);
+void measure_capacitance(void);
+void measure_inductance(void);
+
+#endif // MEASURE_H
