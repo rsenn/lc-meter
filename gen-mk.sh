be2312345f2e918b6d73149727234fa9c00a5a30
Roman Senn <roman.l.senn@gmail.com>
2019-09-15 23:54:28 +0200

    ...

--- gen-mk.sh
+++ gen-mk.sh
@@ -1,1 +1,1 @@
-genmakefile -t xc8 -m gmake -D__XC=1 -DUSE_UART=1 -DUSE_TIMER{0,1,2}=1 -DUSE_HD44780_LCD=1 -I. -Ilib -Isrc lib/*.c src/*.c LC-meter.? -o xc8.mk  --no-create-libs
+genmakefile -t xc8 -m gmake -D__XC=1 -DUSE_UART=1 -DUSE_TIMER{0,1,2}=1 -DUSE_HD44780_LCD=1 -I. -Ilib -Isrc lib/*.c src/*.c LC-meter.? --no-create-libs -o xc8.mk  
--- src/config-18f2550.h
+++ src/config-18f2550.h
@@ -3,16 +3,16 @@
 
 #if defined(__XC)
 
-//#pragma config FOSC = HSPLL_HS
-//#pragma config PWRT = ON, BOR = ON, BORV = 27
-//#pragma config WDT = OFF
-//#pragma config CCP2MX = ON, STVREN = OFF, LVP = OFF
-//#pragma config CP0 = OFF, CP1 = OFF, CP2 = OFF, CP3 = OFF
-//#pragma config CPB = OFF, CPD = OFF
-//#pragma config WRT0 = OFF, WRT1 = OFF, WRT2 = OFF, WRT3 = OFF
-//#pragma config WRTB = OFF, WRTC = OFF, WRTD = OFF
-//#pragma config EBTR0 = OFF, EBTR1 = OFF, EBTR2 = OFF, EBTR3 = OFF
-//#pragma config EBTRB = OFF
+#pragma config FOSC = HSPLL_HS
+#pragma config PWRT = ON, BOR = ON, BORV = 27
+#pragma config WDT = OFF
+#pragma config CCP2MX = ON, STVREN = OFF, LVP = OFF
+#pragma config CP0 = OFF, CP1 = OFF, CP2 = OFF, CP3 = OFF
+#pragma config CPB = OFF, CPD = OFF
+#pragma config WRT0 = OFF, WRT1 = OFF, WRT2 = OFF, WRT3 = OFF
+#pragma config WRTB = OFF, WRTC = OFF, WRTD = OFF
+#pragma config EBTR0 = OFF, EBTR1 = OFF, EBTR2 = OFF, EBTR3 = OFF
+#pragma config EBTRB = OFF
 //
 #elif defined(HI_TECH_C)
 //
