4c16d887db059173fadd78c35761b890529a1384
Roman Senn <roman.l.senn@gmail.com>
2019-09-16 10:06:04 +0200

    ...

--- gen-mk.sh
+++ gen-mk.sh
@@ -1,10 +1,12 @@
-for COMPILER in xc8 htc sdcc; do
-	genmakefile -t $COMPILER -m gmake \
-		-DUSE_{UART,HD44780_LCD,TIMER{0,1,2}}=1 \
-		"$@" \
-		-I. -Ilib -Isrc \
-		lib src LC-meter.c \
-		--no-create-libs \
-		--debug \
-		-o $COMPILER.mk
+for BUILD_TYPE in debug release; do
+	for COMPILER in xc8 htc sdcc; do
+		genmakefile -t $COMPILER -m gmake \
+			-DUSE_{UART,HD44780_LCD,TIMER{0,1,2}}=1 \
+			"$@" \
+			-I. -Ilib -Isrc \
+			lib src LC-meter.c \
+			--no-create-libs \
+			--$BUILD_TYPE \
+			-o $COMPILER-$BUILD_TYPE.mk
+	done
 done
