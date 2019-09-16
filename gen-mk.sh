0e1bbb9141c751900c1159d4c1bb4d4341468f9c
Roman Senn <roman.l.senn@gmail.com>
2019-09-16 10:02:11 +0200

    ...

--- gen-mk.sh
+++ gen-mk.sh
@@ -1,26 +1,10 @@
-genmakefile -t xc8 -m gmake \
-	-DUSE_{UART,HD44780_LCD,TIMER{0,1,2}}=1 \
-	"$@" \
-	-I. -Ilib -Isrc \
-	lib src LC-meter.c \
-	--no-create-libs \
-	--debug \
-	-o xc8.mk 
-
-genmakefile -t htc -m gmake \
-	-DUSE_{UART,HD44780_LCD,TIMER{0,1,2}}=1 \
-	"$@" \
-	-I. -Ilib -Isrc \
-	lib src LC-meter.c \
-	--no-create-libs \
-	--debug \
-	-o htc.mk 
-
-genmakefile -t sdcc -m gmake \
-	-DUSE_{UART,HD44780_LCD,TIMER{0,1,2}}=1 \
-	"$@" \
-	-I. -Ilib -Isrc \
-	lib src LC-meter.c \
-	--no-create-libs \
-	--debug \
-	-o sdcc.mk 
+for COMPILER in xc8 htc sdcc; do
+	genmakefile -t $COMPILER -m gmake \
+		-DUSE_{UART,HD44780_LCD,TIMER{0,1,2}}=1 \
+		"$@" \
+		-I. -Ilib -Isrc \
+		lib src LC-meter.c \
+		--no-create-libs \
+		--debug \
+		-o $COMPILER.mk
+done
