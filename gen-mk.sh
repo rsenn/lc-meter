ff6bdc4a027c91d889aa227c34046207f950864a
Roman Senn <roman.l.senn@gmail.com>
2019-09-19 02:21:03 +0200

    ...

--- gen-mk.sh
+++ gen-mk.sh
@@ -6,7 +6,7 @@ case "$OS" in
  esac
 
 set -- --preproc="./cpp-xc8${EXEEXT} -DMCHP_XC8=1 -D__XC=1" "$@" 
-for CHIP in 16f876a 18f2455 18f2520 18f2550 18f25k22 18f25k50; do
+for CHIP in 16f876a 18f2450 18f2520 18f2550 18f25k22 18f25k50; do
 	for BUILD_TYPE in debug release; do
 		for COMPILER in xc8 htc #sdcc
 		do
