7a9144380f52e6d81db04701ff99e5cf00a9028e
Roman Senn <roman.l.senn@gmail.com>
2019-11-27 18:28:01 +0100

    ...

---
+++ build/mplabx-pic16f/LC_meter_16f876a_xc8.X/build/release/production/_ext/1386521430/format.p1.d
@@ -0,0 +1,6 @@
+ build/release/production/_ext/1386521430/format.d  \
+ build/release/production/_ext/1386521430/format.p1:  \
+ ../../../lib/format.c  \
+../../../lib/format.h  \
+../../../lib/device.h  \
+../../../lib/typedef.h 
