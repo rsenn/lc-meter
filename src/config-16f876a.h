2c8081b1a322c42c53336eafcd39d55804e57b4b
Roman <roman.l.senn@gmail.com>
2020-01-12 04:30:58 +0100

    ...

--- src/config-16f876a.h
+++ src/config-16f876a.h
@@ -4,10 +4,17 @@
 #ifdef __XC
 
 #pragma config FOSC = HS, LVP = OFF, CPD = OFF, BOREN = OFF, WDTE = OFF
+#ifdef DEBUG
+#pragma config DEBUG = ON
+#endif
 
 #elif defined(HI_TECH_C)
 
+#ifdef DEBUG
+__CONFIG(FOSC_HS& WDTE_OFF& PWRTE_ON& LVP_OFF& CPD_OFF& DEBUG_ON&CP_OFF);
+#else
 __CONFIG(FOSC_HS& WDTE_OFF& PWRTE_ON& LVP_OFF& CPD_OFF& DEBUG_OFF& CP_OFF);
+#endif
 
 #endif
 
