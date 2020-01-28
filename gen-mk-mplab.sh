1b4c910dd5b2c84707227c6c211dcf1d47efbac9
Roman <roman.l.senn@gmail.com>
2020-01-28 17:59:06 +0100

    ...

--- gen-mk-mplab.sh
+++ gen-mk-mplab.sh
@@ -21,15 +21,13 @@ BUILD_TYPES='debug'
 : ${PROGRAM_NAME=${SOURCES%%.*}}
 : ${PROGRAMS:='LC-meter Freq-meter Cap-meter'}
 for PROGRAM_NAME in $PROGRAMS; do 
-  (eval "SOURCES=\$(set -- $SOURCES; echo \$(ls -d \$@) 2>/dev/null)"
-  echo "SOURCES='$SOURCES'" 1>&2
   for CHIP in $CHIPS;  do
     for BUILD_TYPE in $BUILD_TYPES; do
-      for COMPILER in $COMPILERS
-      do
-
-      eval "(set -x;	genmakefile -t "$COMPILER" -m mplab $@ -I. -Ilib -Isrc "$SOURCES" -DHAVE_COMPARATOR=1 -DSER_TIMER=2 -DUART_BAUD=38400 -DUSE_SOFTPWM=1 -DUSE_SER=1 -DUSE_TIMER0=1 -DUSE_TIMER1=1 -DUSE_TIMER2=1 -D_XTAL_FREQ=20000000 --create-bins --no-create-libs --$BUILD_TYPE --chip=${CHIP} -o build/mplab/$PROGRAM_NAME-$CHIP-$COMPILER-$BUILD_TYPE.mcp >&genmakefile-$COMPILER-$BUILD_TYPE.log) 2>&10"
+      for COMPILER in $COMPILERS; do
+       (eval "SOURCES=\$(set -- $SOURCES; echo \$(ls -d \$@) 2>/dev/null)"
+        echo "SOURCES='$SOURCES'" 1>&2
+        eval "(set -x;	genmakefile -t "$COMPILER" -m mplab $@ -I. -Ilib -Isrc "$SOURCES" -DHAVE_COMPARATOR=1 -DSER_TIMER=2 -DUART_BAUD=38400 -DUSE_SOFTPWM=1 -DUSE_SER=1 -DUSE_TIMER0=1 -DUSE_TIMER1=1 -DUSE_TIMER2=1 -D_XTAL_FREQ=20000000 --create-bins --no-create-libs --$BUILD_TYPE --chip=${CHIP} -d obj/${COMPILER}-${CHIP}/$BUILD_TYPE/ -o build/mplab/$PROGRAM_NAME-$CHIP-$COMPILER-$BUILD_TYPE.mcp >&genmakefile-$COMPILER-$BUILD_TYPE.log) 2>&10")
       done
     done
-  done)
+  done
 done
