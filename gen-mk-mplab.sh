9c0477eb636fdc93ba7b5fd89c1814a2336f384a
Roman <roman.l.senn@gmail.com>
2020-01-28 17:30:53 +0100

    ...

--- gen-mk-mplab.sh
+++ gen-mk-mplab.sh
@@ -6,12 +6,13 @@ case "$OS" in
   "GNU/Linux") EXEEXT=".elf" ;;
   *) EXEEXT=".exe" ;;
  esac
-IFS="
+IFS=" ""
  	"
 : ${COMPILERS:="xc8 htc sdcc"}
 [ $# -gt 0 ] && SOURCES="$*" ||
-SOURCES='LC-meter.? lib/{comparator,format,random,ser,softpwm,timer,uart}.? src/config-${CHIP}.h src/config-bits.h src/*.c src/pictest.h'
+SOURCES='LC-meter.? lib/{comparator,format,random,ser,softpwm,timer,uart}.? src/config-${CHIP}.h src/config-bits.h src/*.c'
 echo "SOURCES='$SOURCES'" 1>&2
+
 set -- --preproc="./cpp-xc8${EXEEXT}" 
 : ${CHIPS='16f876a 18f2450 18f2520 18f2550 18f25k22 18f25k50'}
 CHIPS='18f25k50 18f2550'
@@ -22,18 +23,9 @@ for CHIP in $CHIPS;  do
 	for BUILD_TYPE in $BUILD_TYPES; do
 		for COMPILER in $COMPILERS
 		do
-      eval "SOURCES=\$(set -- $SOURCES; ls -d \$@)"
+      eval "SOURCES=\$(set -- $SOURCES; echo \$(ls -d \$@))"
 
-		eval \(set -x\;	genmakefile -t $COMPILER -m mplab \
-				"$@" \
-				-I. -Ilib -Isrc \
-		${SOURCES} \
-      -DHAVE_COMPARATOR=1 -DSOFTSER_TIMER=2 -DUART_BAUD=38400 -DUSE_SOFTPWM=1 -DUSE_SOFTSER=1 -DUSE_TIMER0=1 -DUSE_TIMER1=1 -DUSE_TIMER2=1 -D_XTAL_FREQ=20000000 \
-				--create-bins \
-				--no-create-libs \
-				--$BUILD_TYPE \
-				--chip=${CHIP} \
-        -o build/mplab/$PROGRAM_NAME-$CHIP-$COMPILER-$BUILD_TYPE.mcp ">&genmakefile-$COMPILER-$BUILD_TYPE.log) 2>&10"
+		eval "(set -x;	genmakefile -t "$COMPILER" -m mplab $@ -I. -Ilib -Isrc "$SOURCES" -DHAVE_COMPARATOR=1 -DSOFTSER_TIMER=2 -DUART_BAUD=38400 -DUSE_SOFTPWM=1 -DUSE_SOFTSER=1 -DUSE_TIMER0=1 -DUSE_TIMER1=1 -DUSE_TIMER2=1 -D_XTAL_FREQ=20000000 --create-bins --no-create-libs --$BUILD_TYPE --chip=${CHIP} -o build/mplab/$PROGRAM_NAME-$CHIP-$COMPILER-$BUILD_TYPE.mcp >&genmakefile-$COMPILER-$BUILD_TYPE.log) 2>&10"
 		done
 	done
 done
