36d4f14c78a21a2cbe356264d9d7fb30533638b0
Roman <roman.l.senn@gmail.com>
2020-02-05 20:25:41 +0100

    ...

--- LC-meter.h
+++ LC-meter.h
@@ -23,10 +23,10 @@
 
 #if PIC16F628
 #define NO_PORTC 1
-#define LC_SELECT (!!(PORTA & 0b00100000)) // L or C select (from DPDT switch)
-#define LC_TRIS() TRISA |= 0b00100000      // as input
-#define ADD_CCAL() OUTA |= 0b01000000      // relay on
-#define REMOVE_CCAL() OUTA &= ~0b01000000  // relay off
+#define LC_SELECT (!!(PORTA &(1 << 5))) // L or C select (from DPDT switch)
+#define LC_TRIS() TRISA |=(1 << 5)      // as input
+#define ADD_CCAL() OUTA |= (1 << 6)      // relay on
+#define REMOVE_CCAL() OUTA &= ~(1 << 6)  // relay off
 #define RELAY_TRIS() TRISA &= ~(1 << 7)    // as output to drive the relay coil
 #elif PIC18F2550
 #define NO_PORTC 0
@@ -37,10 +37,10 @@
 #define RELAY_TRIS() TRISC &= ~(1 << 5) // as output to drive the relay coil
 #else
 #define NO_PORTC 0
-#define LC_SELECT (!!(PORTC & 0b10000)) // L or C select (from DPDT switch)
-#define LC_TRIS() TRISC |= (1 << 4)     // as input
-#define ADD_CCAL() OUTC |= (1 << 5)     // relay on
-#define REMOVE_CCAL() OUTC &= ~(1 << 5) // relay off
+#define LC_SELECT (!!(PORTC & (1 << 4))) // L or C select (from DPDT switch)
+#define LC_TRIS() TRISC |= (1 << 4)     // RC4 as input
+#define ADD_CCAL() OUTC |= (1 << 5)     // RC5=1 relay on 
+#define REMOVE_CCAL() OUTC &= ~(1 << 5) // RC5=0 relay off
 #define RELAY_TRIS() TRISC &= ~(1 << 5) // as output to drive the relay coil
 
 #endif
@@ -49,16 +49,16 @@
 #define LED2_ANODE_TRIS TRISC1
 */
 #ifdef __16f628
-#define INIT_LED() TRISA &= ~0b00010000
+#define INIT_LED() TRISA &= ~(1 << 4)
 #define SET_LED(b)                                                                                                     \
-  OUTA &= ~0b00010000;                                                                                                 \
+  OUTA &= ~(1 << 4);                                                                                                 \
   OUTA |= (!!(b)) << 4;
 #else
-#define INIT_LED() TRISC &= ~0b00000100
+#define INIT_LED() TRISC &= ~(1 << 2)
 #define SET_LED(b)                                                                                                     \
-  OUTC &= ~0b00000100;                                                                                                 \
+  OUTC &= ~(1 << 2);                                                                                                 \
   OUTC |= (!!(b)) << 2;
-#define TOGGLE_LED() OUTC ^= 0b00000100;
+#define TOGGLE_LED() OUTC ^= (1 << 2);
 #endif
 
 //#define TIMER0_PRESCALER PRESCALE_1_32
