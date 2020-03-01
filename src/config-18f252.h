2038ec6c74c73149107d9c713ca3acc442d36ed2
Roman <roman.l.senn@gmail.com>
2020-03-01 01:54:47 +0100

    ...

--- src/config-18f252.h
+++ src/config-18f252.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_18F252_H
 #define CONFIG_18F252_H 1
 
-#if defined(__XC) || defined(SDCC)
+#if defined(__XC) || defined(MCHP_XC8) || defined(SDCC)
 
 #pragma config OSC = HSPLL, OSCS = OFF
 #pragma config PWRT = ON, BOR = ON, BORV = 27
@@ -10,19 +10,14 @@
 #pragma config STVR = OFF
 #pragma config LVP = OFF
 #pragma config CPD = OFF
-/*#pragma config CPB = OFF
-#pragma config CP0 = OFF, CP1 = OFF, CP2 = OFF, CP3 = OFF
-#pragma config WRT0 = OFF, WRT1 = OFF, WRT2 = OFF, WRT3 = OFF
-#pragma config WRTB = OFF, WRTC = OFF, WRTD = OFF
-#pragma config EBTR0 = OFF, EBTR1 = OFF, EBTR2 = OFF, EBTR3 = OFF
-#pragma config EBTRB = OFF
-*/
+#pragma config WRT0 = OFF, WRT1 = OFF, WRT2 = OFF, WRT3 = OFF, WRTC = OFF, WRTB = OFF, WRTD = OFF, EBTR0 = OFF, EBTR1 = OFF, EBTR2 = OFF, EBTR3 = OFF, EBTRB = OFF
+
 
 #elif defined(HI_TECH_C)
 #include <htc.h>
 
-__CONFIG(1, OSC_HSPLL& OSCS_OFF);
-__CONFIG(2, BOR_ON& BORV_27& PWRT_ON& WDT_OFF);
+__CONFIG(1, OSC_HSPLL & OSCS_OFF);
+__CONFIG(2, BOR_ON & BORV_27 & PWRT_ON & WDT_OFF);
 __CONFIG(3, CCP2MUX_ON);
 __CONFIG(4, LVP_OFF);
 __CONFIG(5, CPD_OFF);
@@ -30,3 +25,4 @@ __CONFIG(5, CPD_OFF);
 #endif
 
 #endif // defined CONFIG_18F252_H
+
